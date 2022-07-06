219cac06c97d4e671854c5f845db3d4952be9469
imagine <88944530+imagine1337@users.noreply.github.com>
2022-07-06 14:51:05 -0500

    Update index.js
    added start menu where you can select amount of circles

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ scores = [];
 const average = (array) => array.reduce((a, b) => a + b) / array.length;
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
     txtbox.innerHTML = '<input type="text" id="inputBox"></input> <br /> <button onclick="evalGuess()">Guess</button>';
 }
 
@@ -50,20 +50,29 @@ function dropMatches(n) {
 }
 
 function nn2() {
-    n2 = Math.floor(Math.random() * 102)
+    n2 = Math.floor(Math.random() * unitNumber)
 }
 
 function evalGuess() {
     guess = parseInt(document.getElementById('inputBox').value)
-    clearCanvas()
+        //clearCanvas()
     scores.push(Math.abs(guess - (n2 + 1)))
     txtbox.innerHTML = `<div id="answers"><h1 id="ans">Answer: ${n2+1}</h1> <br /> <h1 id="ans">Guess: ${guess}</h1> <br /> <h1 id="ans">Difference: ${Math.abs(guess-(n2+1))}</h1> <br /> <h1 id="ans">Score: ${average(scores)}</h1></div>`
     nn2()
+    clearCanvas()
     canvas.width = document.body.clientWidth - 24;
     canvas.height = document.body.clientHeight - 24;
     setTimeout(dropMatches, 5400, n2)
 }
 
-let n2 = Math.floor(Math.random() * 102);
+txtbox.innerHTML = '<div id="answers"> <input type="text" id="n"></input> <br /> <button onclick="start()">Start</button></div>'
 
-dropMatches(n2)
+
+
+function start() {
+    globalThis.unitNumber = parseInt(document.getElementById('n').value);
+    console.log(unitNumber)
+    txtbox.innerHTML = ''
+    globalThis.n2 = Math.floor(Math.random() * unitNumber);
+    dropMatches(n2)
+}
