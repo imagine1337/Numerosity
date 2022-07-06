fd157ef4730a7278aeeed760a21d7554edb9d39d
imagine <88944530+imagine1337@users.noreply.github.com>
2022-07-06 01:14:05 -0500

    Update index.js
    fully functional

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ inpbox = document.getElementById('inputBox');
 
 scores = [];
 
+const average = (array) => array.reduce((a, b) => a + b) / array.length;
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     txtbox.innerHTML = '<input type="text" id="inputBox"></input> <br /> <button onclick="evalGuess()">Guess</button>';
@@ -26,6 +28,7 @@ function randint(n) {
 }
 
 function dropMatch() {
+    txtbox.innerHTML = ''
     x = randint(canvas.width)
     y = randint(canvas.height)
     n0 = randint(72)
@@ -46,13 +49,21 @@ function dropMatches(n) {
     setTimeout(draw, 3000)
 }
 
-let n2 = Math.floor(Math.random() * 102);
+function nn2() {
+    n2 = Math.floor(Math.random() * 102)
+}
 
 function evalGuess() {
     guess = parseInt(document.getElementById('inputBox').value)
     clearCanvas()
-    txtbox.innerHTML = `<h1 id="ans">Answer: ${n2-1}</h1> <br /> <h1 id="ans">Guess: ${guess}</h1> <br /> <h1 id="ans">Difference: ${Math.abs(guess-(n2-1))}</h1>`
+    scores.push(Math.abs(guess - (n2 + 1)))
+    txtbox.innerHTML = `<div id="answers"><h1 id="ans">Answer: ${n2+1}</h1> <br /> <h1 id="ans">Guess: ${guess}</h1> <br /> <h1 id="ans">Difference: ${Math.abs(guess-(n2+1))}</h1> <br /> <h1 id="ans">Score: ${average(scores)}</h1></div>`
+    nn2()
+    canvas.width = document.body.clientWidth - 24;
+    canvas.height = document.body.clientHeight - 24;
+    setTimeout(dropMatches, 5400, n2)
 }
 
-console.log(n2)
+let n2 = Math.floor(Math.random() * 102);
+
 dropMatches(n2)
